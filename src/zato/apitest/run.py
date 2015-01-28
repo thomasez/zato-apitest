009fbfec6dcd6a7a6fffeb6636d43b3a4f00411d
Thomas Lundquist <github@bisonlab.no>
2015-01-28 16:57:14 +0100

    This will add the (Ehhh, hidden?) option "-b" or "--behave_options" which will add the values to behave_options from the config.ini before behave is run. I made this to support the --tags options in Behave.
    Usage-example:
        apitest --behave_options="--tags=thisone" run .

--- src/zato/apitest/run.py
+++ src/zato/apitest/run.py
@@ -13,6 +13,23 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 # stdlib
 import os
+import sys
+import re
+
+global extra_behave_options
+extra_behave_options = ''
+args_parsed = []
+for idx, arg in enumerate(sys.argv):
+    if re.match("--behave_options", arg):
+	extra_behave_options += " " + re.sub(r'--behave_options=', '', arg)
+	args_parsed.append(arg)
+    if re.match("-b", arg):
+	extra_behave_options += " " + sys.argv[idx + 1]
+	args_parsed.append(arg)
+	args_parsed.append(sys.argv[idx + 1])
+
+for arg in args_parsed:
+    sys.argv.remove(arg)
 
 # Behave
 from behave.configuration import Configuration
@@ -23,7 +40,7 @@ from configobj import ConfigObj
 
 def handle(path):
     file_conf = ConfigObj(os.path.join(path, 'features', 'config.ini'))
-    behave_options = file_conf['behave']['options']
+    behave_options = file_conf['behave']['options'] + extra_behave_options
 
     conf = Configuration(behave_options)
     conf.paths = [os.path.join(path, 'features')]
